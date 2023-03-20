b45cdb932232b2b66ac168c20bc9a0d15e2793c0
Aayush Bhan <63007327+Bhan-Aayush@users.noreply.github.com>
2023-03-19 20:34:54 -0400

    Add files via upload

---
+++ a3/a3_part2.py
@@ -0,0 +1,181 @@
+"""CSC110 Fall 2021 Assignment 3, Part 2: Text Generation, One-Word Context Model (SOLUTIONS)
+
+Instructions (READ THIS FIRST!)
+===============================
+Implement each of the functions in this file. As usual, do not change any function headers
+or preconditions. You do NOT need to add doctests.
+
+You may create some additional helper functions to help break up your code into smaller parts.
+
+Copyright and Usage Information
+===============================
+
+This file is provided solely for the personal and private use of students
+taking CSC110 at the University of Toronto St. George campus. All forms of
+distribution of this code, whether as given or with any changes, are
+expressly prohibited. For more information on copyright for CSC110 materials,
+please consult our Course Syllabus.
+
+This file is Copyright (c) 2021 Mario Badr and Tom Fairgrieve.
+"""
+import random
+
+
+###############################################################################
+# Question 2
+###############################################################################
+def update_follow_list(model: dict[str, list[str]], word: str, follow_word: str) -> None:
+    """Add follow_word and, when applicable, word to model.
+
+    If word is not already present in model, add it to the model with the follow list
+    [follow_word]. Otherwise, add follow_word to the follow list of word.
+    """
+    if word not in model:
+        follow_list = [follow_word]
+        model[word] = follow_list
+    else:
+        list.append(model[word], follow_word)
+
+
+def create_model_owc(text: str) -> tuple[int, dict[str, list[str]]]:
+    """Return a tuple of the number of words in text and one-word context model of the given text,
+    as described in the handout.
+
+    Your implementation MUST use the update_follow_list helper function. We recommend completing
+    that function first, as it's simpler and will get you thinking about how to use it here.
+
+    Preconditions:
+        - text != ''
+        - len(str.split(text)) > 1
+    """
+    split_text = str.split(text)
+    text_counter_without_last_word = 1
+    context_model = {}
+
+    for i in range(0, len(split_text) - 1):
+        text_counter_without_last_word = text_counter_without_last_word + 1
+        update_follow_list(context_model, split_text[i], split_text[i + 1])
+
+    return (text_counter_without_last_word, context_model)
+
+
+###############################################################################
+# Question 3
+###############################################################################
+def choose_from_keys(transitions: dict[str, list[str]]) -> str:
+    """Return a random key from transitions.
+
+    Preconditions:
+        - transitions != {}
+    """
+    keys_choice_list = list(transitions.keys())
+    return random.choice(keys_choice_list)
+
+
+def choose_from_follow_list(key: str, transitions: dict[str, list[str]]) -> str:
+    """Return a random word from the follow list in transitions that is associated with key.
+
+    Also remove one occurrence of the random word from the follow list. If the follow list is then
+    empty, remove the key-value pair from transitions.
+
+    Preconditions:
+        - transitions != {}
+        - key in transitions
+        - transitions[key] != []
+    """
+    follow_choice_index = random.choice(range(0, len(transitions[key])))
+    follow_choice = transitions[key][follow_choice_index]
+    follow_choice_return_val = follow_choice
+
+    if transitions[key] == []:
+        transitions.pop(key)
+
+    transitions[key].pop(follow_choice_index)
+
+    return follow_choice_return_val
+
+
+def generate_text_owc(count: int, transitions: dict[str, list[str]]) -> str:
+    """Return a string containing (count - 1) randomly generated words based on the data in
+    transitions, which maps words to a list of words that follow it.
+
+    A randomly generated word is selected from the keys of transitions when:
+        - it is the first word; or
+        - the last randomly generated word is not a key in transitions.
+
+    A randomly generated word is selected from the follow list of a key in transitions when the
+    last randomly generated word is a key in transitions. In addition, one occurrence of the word
+    selected from the follow list is removed from the follow list (i.e., mutation). When there are
+    no words in the follow list for a key, the key-value pair is also removed from transitions
+    (i.e., mutation).
+
+    Your implementation MUST use the helper functions: choose_from_keys and choose_from_follow_list.
+    We recommend completing these functions first, as they simpler and will get you thinking about
+    how to use it here.
+
+    Preconditions:
+        - model is in the format described by the assignment handout
+    """
+    # ACCUMULATOR: a list of the randomly-generated words so far
+    words_so_far = []
+
+    # We've provided this template as a starting point; you may modify it as necessary.
+    next_word = ''
+
+    for i in range(0, count - 1):
+        if i == 0:
+            chosen_key = choose_from_keys(transitions)
+            list.append(words_so_far, chosen_key)
+            next_word = transitions[words_so_far[i]][0]
+        elif i < count - 1:
+            if words_so_far[i - 1] not in transitions:
+                chosen_key = next_word
+            elif len(transitions[words_so_far[i - 1]]) == 1:
+                next_word = transitions[words_so_far[i - 1]][0]
+                chosen_key = words_so_far[i - 1]
+            else:
+                chosen_key = words_so_far[i - 1]
+            chosen_transition = choose_from_follow_list(chosen_key, transitions)
+            list.append(words_so_far, chosen_transition)
+        else:
+            var = [transitions[x] for x in transitions]
+            chosen_transition = var[0]
+            list.append(words_so_far, chosen_transition)
+
+    return str.join(' ', words_so_far)
+
+
+def run_example(filename: str) -> str:
+    """Run an example to demonstrate random text generation based on the data in filename.
+
+    To call this function:
+        - Make sure you see that the 'data' folder is in the same directory as this file
+        - Use an argument for filename like: 'data/texts/sample_text_raw.txt'
+        - Try out the other plaintext files in  'data/texts', too
+    """
+    with open(filename) as f:
+        file_text = f.read()
+
+    stripped_text = str.strip(file_text)  # str.strip removes leading/trailing whitespace
+    word_count, transition_model = create_model_owc(stripped_text)
+    generated_words = generate_text_owc(word_count, transition_model)
+
+    return generated_words
+
+
+if __name__ == '__main__':
+    import python_ta
+    import python_ta.contracts
+    python_ta.contracts.DEBUG_CONTRACTS = False
+    python_ta.contracts.check_all_contracts()
+
+    # When you are ready to check your work with python_ta, uncomment the following lines.
+    # (Delete the "#" and space before each line.)
+    # IMPORTANT: keep this code indented inside the "if __name__ == '__main__'" block
+    # python_ta.check_all(config={
+    #     'allowed-io': ['run_example'],
+    #     'extra-imports': ['python_ta.contracts', 'random'],
+    #     'max-line-length': 100,
+    #     'max-nested-blocks': 4,
+    #     'disable': ['R1705', 'C0200']
+    # })
---
+++ a3/a3_part4_tests.py
@@ -0,0 +1,130 @@
+"""CSC110 Fall 2021 Assignment 3: Part 4 (Student Test Suite)
+
+Instructions (READ THIS FIRST!)
+===============================
+Complete the unit tests in this file based on their docstring descriptions.
+
+Copyright and Usage Information
+===============================
+
+This file is provided solely for the personal and private use of students
+taking CSC110 at the University of Toronto St. George campus. All forms of
+distribution of this code, whether as given or with any changes, are
+expressly prohibited. For more information on copyright for CSC110 materials,
+please consult our Course Syllabus.
+
+This file is Copyright (c) 2021 Mario Badr and Tom Fairgrieve.
+"""
+import pytest
+
+from a3_part4 import load_data
+from a3_ffwi_system import WeatherMetrics, FfwiOutput
+
+import a3_ffwi_system as ffwi
+
+
+class TestCalculateMr:
+    """A collection of unit tests for calculate_mr."""
+
+    def test_equation_3a_branch(self) -> None:
+        """Test the branch calculate_mr that contains Equation 3a."""
+        assert ffwi.calculate_mr(1.0, 150.0) <= 157.89521538860947
+
+    def test_equation_3b_branch(self) -> None:
+        """Test the branch calculate_mr that contains Equation 3b."""
+        assert ffwi.calculate_mr(1.0, 150.000000000001) > 157.89521538860947
+
+
+class TestCalculateM:
+    """A collection of unit tests for calculate_m."""
+
+    def test_no_mutation_mo_equals_ed(self) -> None:
+        """Test that calculate_m does not mutate the WeatherMetrics argument when mo == ed."""
+        wm = WeatherMetrics(1, 1, 1.0, 1.0, 1.0, 1.0)
+        ffwi.calculate_m(wm, 1.0, 1.0)
+
+        assert wm == WeatherMetrics(1, 1, 1.0, 1.0, 1.0, 1.0)
+
+    def test_no_mutation_mo_leq_ew(self) -> None:
+        """Test that calculate_m does not mutate the WeatherMetrics argument when mo <= ew."""
+        wm = WeatherMetrics(1, 1, 1.0, 1.0, 1.0, 1.0)
+        ffwi.calculate_m(wm, 2.0, 1.0)
+
+        assert wm == WeatherMetrics(1, 1, 1.0, 1.0, 1.0, 1.0)
+
+    def test_no_mutation_mo_greater_than_ew(self) -> None:
+        """Test that calculate_m does not mutate the WeatherMetrics argument when mo > ew."""
+        wm = WeatherMetrics(1, 1, 1.0, 1.0, 1.0, 1.0)
+        ffwi.calculate_m(wm, 2.0, 1.5)
+
+        assert wm == WeatherMetrics(1, 1, 1.0, 1.0, 1.0, 1.0)
+
+    def test_no_mutation_mo_greater_than_ed(self) -> None:
+        """Test that calculate_m does not mutate the WeatherMetrics argument when mo > ed."""
+        wm = WeatherMetrics(1, 1, 1.0, 1.0, 1.0, 1.0)
+        ffwi.calculate_m(wm, 1.0, 2.0)
+
+        assert wm == WeatherMetrics(1, 1, 1.0, 1.0, 1.0, 1.0)
+
+
+@pytest.fixture
+def sample_data() -> tuple[list[WeatherMetrics], list[FfwiOutput]]:
+    """A pytest fixture containing the data in data/ffwi/sample_data.csv
+
+    NOTE: Do not change this function. Do not call this function directly. It is a pytest fixture,
+    so pytest will call it automatically and pass it to test_ffmc_against_ground_truth below.
+    """
+    return load_data('data/ffwi/sample_data.csv')
+
+
+def test_ffmc_against_ground_truth(sample_data) -> None:
+    """Test the correctness of calculate_ffmc, calculate_dmc, calculate_dc, calculate_isi,
+     calculate_bui, and calculate_fwi based on sample_data.
+
+    Ensure that, for every WeatherMetric element in sample_data[0] passed to each of the calculate_
+    functions mentioned above, the return value, rounded to the nearest decimal, matches the
+    corresponding value from the FfwiOutput element in sample_data[1].
+
+    Hints:
+        - You will need to use the built-in function round.
+        - You may want to use pytest.approx since you are comparing float values.
+    """
+    ffmc = ffwi.INITIAL_FFMC
+    dmc = ffwi.INITIAL_DMC
+    dc = ffwi.INITIAL_DC
+
+    inputs, outputs = sample_data
+    ffmc_list = [ffwi.calculate_ffmc(x, ffmc) for x in inputs]
+    dmc_list = [ffwi.calculate_dmc(x, dmc) for x in inputs]
+    dc_list = [ffwi.calculate_dc(x, dc) for x in inputs]
+    isi_list = [ffwi.calculate_isi(x, y) for x in inputs for y in ffmc_list]
+    bui_list = [ffwi.calculate_bui(x, y) for x in dmc_list for y in dc_list]
+    fwi_list = [ffwi.calculate_fwi(x, y) for x in isi_list for y in bui_list]
+
+    for i in range(len(ffmc_list)):
+        assert round(ffmc_list[i]) == pytest.approx(outputs[i].ffmc)
+        assert round(dmc_list[i]) == pytest.approx(outputs[i].dmc)
+        assert round(dc_list[i]) == pytest.approx(outputs[i].dc)
+        assert round(isi_list[i]) == pytest.approx(outputs[i].isi)
+        assert round(bui_list[i]) == pytest.approx(outputs[i].bui)
+        assert round(fwi_list[i]) == pytest.approx(outputs[i].fwi)
+
+
+if __name__ == '__main__':
+    pytest.main(['a3_part4_tests.py'])
+
+    # When you are ready to check your work with python_ta, uncomment the following lines.
+    # (Delete the "#" and space before each line.)
+    # IMPORTANT: keep this code indented inside the "if __name__ == '__main__'" block
+    # import python_ta
+    # import python_ta.contracts
+    # python_ta.contracts.DEBUG_CONTRACTS = False
+    # python_ta.contracts.check_all_contracts()
+    # python_ta.check_all(config={
+    #     'allowed-io': ['load_data'],
+    #     'extra-imports': ['a3_ffwi_system', 'a3_part4', 'pytest'],
+    #     'max-line-length': 100,
+    #     'max-args': 6,
+    #     'max-locals': 25,
+    #     'disable': ['R1705', 'R0201', 'C0103', 'W0621', 'E9970'],
+    # })
